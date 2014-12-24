caacc765e29fe00744098a4b94fc54b413e143be
waplet <maarisjankovskis@gmail.com>
2014-12-24 18:11:59 +0200

    backup code, ftw

--- g.php
+++ g.php
@@ -1,128 +1,10 @@
 <?php
-
-/**
- * Creating Stack automata
- *
- * First starting, by creating simple automata
- * Seconds - automata created, proceed with magazine
- */
-
-class Automata {
-
-    private $states            = [];
-    private $terminals         = [];
-    private $stack_symbols     = [];
-    private $start_state       = ''; // probably name, probably index
-    private $start_symbol      = '';
-    private $acceptable_states = [];
-    private $search_word       = '';
-    private $delta_function    = '';
-    private $current_state     = '';
-
-    private $emptity_symbol = 'e';
-    private $found          = false;
-
-    function __construct() {
-        return;
-    }
-
-    public function setStates( array $states ) {
-        $this->states = $states;
-    }
-    public function setTerminals( array $terminals ) {
-        $this->terminals = $terminals;
-    }
-    public function setStackSymbols( array $stack_symbols ) {
-        $this->stack_symbols = $stack_symbols;
-    }
-    public function setStartState( $start_state = '') {
-        if( !in_array( $start_state, $this->states ) ) throw new Exception( "Incorrect start state" );
-        $this->start_state = $start_state;
-    }
-    public function setStartSymbol( $start_symbol = '') {
-        if( !in_array( $start_symbol, $this->stack_symbols ) ) throw new Exception( "Incorrect stack start symbol" );
-        $this->start_symbol  = $start_symbol;
-    }
-    public function setAcceptableStates( array $acceptable_states ) {
-        $this->acceptable_states = $acceptable_states;
-    }
-    public function setSearchWord( $search_word = '' ) {
-        if( strlen(str_replace( $this->terminals, '', $search_word ) ) > 0) throw new Exception( "Word has incorrect terminals");
-        $this->search_word = $search_word;
-    }
-    public function setDeltaFunction( array $delta_function ) {
-        $this->delta_function = $delta_function;
-    }
-    public function getSearchWord() {
-        return $this->search_word;
-    }
-    public function status() {
-        return $this->found;
-    }
-
-    public function start() {
-        if( empty( $this->start_state ) ) return;
-        $this->checkWord( $this->search_word, $this->start_state, $this->start_symbol );
-        return;
-    }
-    public function checkWord( $word = '', $current_state = '',  $stack = '' ) {
-
-        var_dump($current_state);
-        var_dump($stack);
-        var_dump($word);
-        echo "<br/>";
-        $letter       = !empty($word) ? $word[0] : '';
-        $word_left    = substr($word, 1); // vārds, vai false... attiecīgi false būs empty vārds, utt.
-
-        $stack_letter = !empty($stack) ? $stack[0] : '';
-        $stack_left   = ( substr($stack, 1) === false ) ? '' : substr( $stack, 1);
-
-        //epsilon parejas
-        foreach( $this->delta_function[ $current_state ] as $df ) {
-            if( empty( $df[0] ) and $stack_letter == $df[1] ) {
-                $this->checkWord( $letter.$word_left, $df[2], $df[3].$stack_left);
-            }
-        }
-        // ja vārds ir tukšš un esam akceptējošā stāvoklī
-        if( empty( $word ) ) {
-            if( in_array( $current_state, $this->acceptable_states ) ) {
-                $this->found = true;
-                return;
-            }
-            // no else, vienkārši vārds nepieder.., metam nost.
-            return;
-        }
-
-        // te vajag epsilon pārjeas izpildīt.
-
-
-        // iegūstam apstrādājamo symbolu
-        // padotais stāvoklis nav pieejamajos
-        if( !is_array( $this->delta_function[ $current_state ] ) ) {
-            return;
-        }
-
-        // pārejas funkcija, neņem vērā stāvokļus, nav īsti vajadzīga
-        foreach( $this->delta_function[ $current_state ] as $df) {
-            if( $df[0] == $letter and $df[1] == $stack_letter) {
-                $this->checkWord( $word_left, $df[2], $df[3].$stack_left );
-            }
-        }
-
-        // var_dump($current_state);
-        // var_dump($stack_letter);
-        // echo "<br/>";
-        // do the epsilon goes
-        return;
-    }
-
-
-}
+require_once("Pushdown_Automata.php");
 
 
 try {
 
-    $a = new Automata();
+    $a = new Pushdown_Automata();
     // test case #1
     // $a->setStates( ['Q1', 'Q2', 'Q3']);
     // $a->setTerminals( ['a','b'] ); // termināļi
@@ -145,8 +27,8 @@ try {
     //     ]
     // ]);
 
-    /*
-    // test case #
+
+    // test case #2
     // $a->setStates( ['Q1', 'Q2', 'Q3']);
     // $a->setTerminals( ['a','b'] );
     // $a->setStackSymbols( ['Z','a','b'] );
@@ -175,7 +57,28 @@ try {
     //     // 'Q3' => [
     //     // ]
     // ]);
-    */
+
+    // test case #3
+    $a->setStates( ['Q1', 'Q2', 'Q3']);
+    $a->setTerminals( ['a','b'] );
+    $a->setStackSymbols( ['Z','a','b'] );
+    $a->setStartState( 'Q1' );
+    $a->setStartSymbol( 'Z' );
+    $a->setAcceptableStates( ['Q3'] );
+    $a->setSearchWord('abba');
+    $a->setDeltaFunction( [
+        'Q1' => [
+            [ 'a', '', 'Q1', 'a'],
+            [ 'b', '', 'Q1', 'b'],
+            [ '' , '', 'Q2', '']
+        ],
+        'Q2' => [
+            [ 'a', 'a', 'Q2', '' ],
+            [ 'b', 'b', 'Q2', ''],
+            [ '', 'Z' , 'Q3', '']
+        ],
+        'Q3' => []
+    ]);
     $a->start();
 
 } catch ( Exception $e ) {
